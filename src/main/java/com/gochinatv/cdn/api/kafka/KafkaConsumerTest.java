e15ee32c6dbace96617a177a6a5ab155972fac4f
zhuhuihui <jackjboss@gmail.com>
2017-03-24 14:31:18 +0800

    kafka 测试创建分区，分区接收消息

--- src/main/java/com/gochinatv/cdn/api/kafka/KafkaConsumerTest.java
+++ src/main/java/com/gochinatv/cdn/api/kafka/KafkaConsumerTest.java
@@ -1,6 +1,8 @@
 package com.gochinatv.cdn.api.kafka;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -8,30 +10,62 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 
+import kafka.common.TopicAndPartition;
+import kafka.consumer.SimpleConsumer;
+
 
 
 public class KafkaConsumerTest {
    
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 	     Properties props = new Properties();
-	     props.put("bootstrap.servers", "192.168.2.150:9092");
-	     props.put("group.id", "test");
+	     props.put("bootstrap.servers", "localhost:9092");
+	     props.put("group.id", "kafka01");
 	     props.put("enable.auto.commit", "true");
 	     props.put("auto.commit.interval.ms", "1000");
 	     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 	     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 	     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-	     consumer.subscribe(Arrays.asList("my-topic"));
+	    
+	     //自动设置offset，消费topic中的全部分区
+	     /*consumer.subscribe(Arrays.asList("spark-test"));
 	     while (true) {
 	         ConsumerRecords<String, String> records = consumer.poll(100);
-	         for (ConsumerRecord<String, String> record : records)
+	         for (ConsumerRecord<String, String> record : records){
 	             System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
-	        
+	         }
+	     }*/
+	     
+	     
+	     //测试手动设置offset
+	     /*consumer.subscribe(Arrays.asList("kafka-test"));
+	     while (true) {
+	         ConsumerRecords<String, String> records = consumer.poll(100);
+	         for (ConsumerRecord<String, String> record : records){
+	             System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+	         }
 	         //下面设置后，导致消费端重复消费，如果不指定offset那么消费之后默认移动offset的位置
-	         TopicPartition partition = new TopicPartition("my-topic", 0);
+	         TopicPartition partition = new TopicPartition("kafka-test", 0);
 	         consumer.seek(partition, 9911063);
+	     }*/
+	     
+	     
+	     /**
+	      * 只消费一个topic分区中的部分分区
+	      */
+	     TopicPartition partition0 = new TopicPartition("spark-test", 0);
+	     TopicPartition partition1 = new TopicPartition("spark-test", 1);
+	     //TopicPartition partition2 = new TopicPartition("spark-test", 2);
+	     consumer.assign(Arrays.asList(partition0,partition1));
+
+	     while (true) {
+	         ConsumerRecords<String, String> records = consumer.poll(100);
+	         for (ConsumerRecord<String, String> record : records){
+	             System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+	         }
 	     }
+	     
 	}
 	
 }
--- src/main/java/com/gochinatv/cdn/api/kafka/KafkaProducerTest.java
+++ src/main/java/com/gochinatv/cdn/api/kafka/KafkaProducerTest.java
@@ -7,11 +7,24 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 
+/**
+ * 
+ * 创建消息 topic
+ * kafka-topics.bat --create --topic spark-test --zookeeper localhost:2181 --partitions 3 --replication-factor 1
+ * 
+ * 查看消息topic
+ * kafka-topics.bat --describe --zookeeper localhost:2181 --topic spark-test 
+ * 
+ * 查看消费情况
+ * kafka-run-class.bat kafka.admin.ConsumerGroupCommand  --bootstrap-server localhost:9092  --describe --group kafka01
+ * @author zhuhh
+ *
+ */
 public class KafkaProducerTest {
    
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		 Properties props = new Properties();
-		 props.put("bootstrap.servers", "192.168.2.150:9092");
+		 props.put("bootstrap.servers", "localhost:9092");
 		 props.put("acks", "all");
 		 props.put("retries", 0);
 		 props.put("batch.size", 16384);
@@ -21,14 +34,25 @@ public class KafkaProducerTest {
 		 props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
 
 		 Producer<String, String> producer = new KafkaProducer<>(props);
-		 
-//		 int i = 0;
+		 int i = 0;
+		 //没有任何分区，默认1个分区，发送消息
 //		 while(true){
-//		     producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)+"-jackjboss"));
+//		     producer.send(new ProducerRecord<String, String>("kafka-test", Integer.toString(i), Integer.toString(i)+"-jackjboss"));
+//		     System.out.println("================send============="+i);
 //		     i = i+1;
 //		 }
-		 producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(100), Integer.toString(100)+"-jackjboss"));
-		 producer.close();
+		 
+		 
+		 //需要使用命令行创建topic并且指定分区数，同时发送消息至分区
+		 while(true){
+			 Thread.sleep(1000L);
+		 producer.send(new ProducerRecord<String, String>("spark-test", 0, Integer.toString(100), Integer.toString(100)+"-jackjboss"));
+		 producer.send(new ProducerRecord<String, String>("spark-test", 1, Integer.toString(200), Integer.toString(200)+"-jackjboss"));
+		 producer.send(new ProducerRecord<String, String>("spark-test", 2, Integer.toString(300), Integer.toString(300)+"-jackjboss"));
+		 i = i+1;
+		 }
+		 //producer.flush();
+		 //producer.close();
 	}
 	
 }
