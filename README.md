930852afa1a2de1e6900912f17ca2d2b8292cce2
zhuhuihui <jackjboss@gmail.com>
2017-06-10 16:38:19 +0800

    添加protobuf支持

--- README.md
+++ README.md
@@ -34,7 +34,10 @@ http://localhost:8088/
 + 参考 com.gochinatv.cdn.api.zk  （ TestZK TestZK2  TestZK3  TestZK4相当于4个节点，启动之后选主）
 
 #### avro 序列化的几种实现方式
-+ 参考 com.gochinatv.cdn.api.avro，src/main/resources/user.avsc
++ 参考 com.gochinatv.cdn.api.avro包    lib: data-collection-0.0.1-SNAPSHOT.jar:/avro/*.avsc
+
+#### protobuf 序列化实现
++ 参考 com.gochinatv.cdn.api.protobuf包  lib: data-collection-0.0.1-SNAPSHOT.jar:/protobuf/*.proto
 
 #### elastic-job
 + 分布式任务框架 com.gochinatv.cdn.api.elasticjob
--- src/main/java/com/gochinatv/cdn/api/protobuf/ProtoBufSingleObjectTest.java
+++ src/main/java/com/gochinatv/cdn/api/protobuf/ProtoBufSingleObjectTest.java
@@ -6,6 +6,23 @@ import com.chsoft.datacollection.entity.protobuf.UserEntity;
 import com.chsoft.datacollection.entity.protobuf.UserEntity.User;
 import com.google.protobuf.InvalidProtocolBufferException;
 
+
+/**
+ * protoc --java_out=./ user-entity.proto
+ * 
+ * ProtoBuf序列化后只有两个字节，跟前面两种简直没法比，但ProtoBuf和其他两种的序列化区别很大，
+ * ProtoBuf为与语言平台无关的，需要编写自定义的proto文件定义数据格式，
+ * 然后用ProtoBuf编译器编译成目标语言：C++、Java、Python等的代理类，对象的序列化、反序列化都需要使用该类来完成；
+ * 
+ * ProtoBuf序列化对象的byte数组由一系列的key-Value组成，
+ * key计算公式为：(field_number<<3)|wire_type、Value：经过编码后的byte，
+ * ProtoBuf使用了Varint、zigzag编码极大的压缩了序列化后byte数组的大小，
+ * 所以当前我们看到的byte数组只有08 08 两个字节。
+ * 
+ * 
+ * @author zhuhuihui
+ *
+ */
 public class ProtoBufSingleObjectTest {
 
 	public static void main(String[] args) throws IOException {
