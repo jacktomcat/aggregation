5be44da7c9dd1f0d3ccc5de45b1978ca72ff5c38
zhuhuihui <jackjboss@gmail.com>
2017-07-26 10:37:26 +0800

    add Instant support

--- src/main/java/com/gochinatv/cdn/api/jdk/jdk8/InstantTest.java
+++ src/main/java/com/gochinatv/cdn/api/jdk/jdk8/InstantTest.java
@@ -2,6 +2,8 @@ package com.gochinatv.cdn.api.jdk.jdk8;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 /**
@@ -21,14 +23,26 @@ public class InstantTest {
         instant.atZone(ZoneId.of("Asia/Shanghai"));
         System.out.println("Instant now = " + instant);
         System.out.println("Instant EPOCH = " + Instant.EPOCH);
-        System.out.println("Instant toEpochMilli = " + instant.toEpochMilli());
+        System.out.println("Instant toEpochMilli = " + instant.toEpochMilli());//系统当前的时间戳
 
         //Instant from timestamp
-        Instant specificTime = Instant.ofEpochMilli(1500978707350L);
+        Instant specificTime = Instant.ofEpochMilli(1501034909184L);
         System.out.println("Specific Time = "+specificTime);
  
         //Duration example
-        //Duration thirtyDay = Duration.ofDays(30);
-        //System.out.println(thirtyDay);
+        Duration thirtyDay = Duration.ofDays(3);
+        System.out.println(thirtyDay);
+        
+        LocalDateTime localDate1 = LocalDateTime.of(2017, 7, 1,1,1,1);
+        LocalDateTime localDate2 = LocalDateTime.of(2017, 7, 1,2,1,1);
+        
+        Duration between = Duration.between(localDate1, localDate2);
+        long millis = between.toMillis();
+        long seconds = between.getSeconds();
+        System.out.println("Duration between millis = "+ millis);
+        System.out.println("Duration between seconds = "+ seconds);
+        
+        Instant plus3Day = instant.plus(thirtyDay);
+        System.out.println("Instant plus 3 days = "+ plus3Day);//加了三天之后的时间戳
 	}
 }
