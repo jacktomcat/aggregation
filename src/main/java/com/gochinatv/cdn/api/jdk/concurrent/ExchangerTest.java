30532c10aa7bc22f0fc1951d0ccab63b468221cf
zhuhuihui <jackjboss@gmail.com>
2017-08-02 18:28:59 +0800

    add exchange support

--- src/main/java/com/gochinatv/cdn/api/jdk/concurrent/ExchangerTest.java
+++ src/main/java/com/gochinatv/cdn/api/jdk/concurrent/ExchangerTest.java
@@ -1,5 +1,9 @@
 package com.gochinatv.cdn.api.jdk.concurrent;
 
+import java.util.List;
+import java.util.concurrent.Exchanger;
+
+import org.junit.Test;
 
 /**
  * 
@@ -8,6 +12,11 @@ package com.gochinatv.cdn.api.jdk.concurrent;
  */
 public class ExchangerTest {
 
-	
+	@Test
+	public void exchanger() {
+		Exchanger<List<Integer>> ex = new Exchanger<>();
+		
+		
+	}
 	
 }
