5e5ac51672f789ed40578d92367e9517b77ddec3
zhuhuihui <jackjboss@gmail.com>
2017-08-20 22:09:09 +0800

    add desc to blockingqueue

--- src/main/java/com/gochinatv/cdn/api/jdk/concurrent/BlockingQueueTest.java
+++ src/main/java/com/gochinatv/cdn/api/jdk/concurrent/BlockingQueueTest.java
@@ -3,6 +3,9 @@ package com.gochinatv.cdn.api.jdk.concurrent;
 
 /**
  * http://286.iteye.com/blog/2297295
+ *
+ * http://www.infoq.com/cn/articles/java-blocking-queue
+ * 
  * @author jacktomcat
  *
  */
