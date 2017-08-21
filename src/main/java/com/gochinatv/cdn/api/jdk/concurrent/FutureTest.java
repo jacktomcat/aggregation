31da2a677f1de1685b4192cd389dbb9a5c4b84d4
zhuhuihui <jackjboss@gmail.com>
2017-08-21 17:12:05 +0800

    update future support

--- src/main/java/com/gochinatv/cdn/api/jdk/concurrent/FutureTest.java
+++ src/main/java/com/gochinatv/cdn/api/jdk/concurrent/FutureTest.java
@@ -1,15 +1,19 @@
 package com.gochinatv.cdn.api.jdk.concurrent;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 /**
  * http://blog.csdn.net/u011734144/article/details/52351841
@@ -18,12 +22,11 @@ import org.junit.Test;
  */
 public class FutureTest {
 
-	
 	ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
 	CompletionService<String> completionService = new ExecutorCompletionService<>(fixedThreadPool);
 	
 	@Test
-	public void test(){
+	public void testAsync(){
 	    
 		String[] names = new String[100];
 		for(int i=0;i<100;i++){
@@ -42,8 +45,6 @@ public class FutureTest {
 			}
 	    }
 	    
-	    
-	    
 	    fixedThreadPool.shutdown();
 	    try {
 			fixedThreadPool.awaitTermination(5, TimeUnit.MINUTES);
@@ -51,6 +52,60 @@ public class FutureTest {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	@Test
+	public void testSync(){
+		String[] names = new String[100];
+		ExecutorService service = Executors.newFixedThreadPool(10);
+		List<Future<String>> result = Lists.newArrayList();
+		for(int i=0;i<100;i++){
+			names[i]="names"+i;
+		}
+		
+	    for(int i=0;i<names.length;i++){
+	    	Future<String> submit = service.submit(expectDbAsync(names[i]));
+	    	result.add(submit);
+	    }
+	    
+	    getResult(result);
+	    
+	    service.shutdown();
+	    try {
+	    	service.awaitTermination(5, TimeUnit.MINUTES);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	  public void getResult(List<Future<String>> fList) {
+	        ExecutorService service = Executors.newSingleThreadExecutor();
+	        service.execute(()->{
+	        	fList.forEach(f->{
+	        		try {
+                        while (true) {
+                            if (f.isDone() && !f.isCancelled()) {
+                                System.out.println( "Result:" + f.get());
+                                break;
+                            }else if(f.isCancelled()){
+                            	System.out.println("任务取消" + ",Result:" + f.get());
+                            } else {
+                                Thread.sleep(1000);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+	        	});
+	        });
+	        service.shutdown();
+	        try {
+				service.awaitTermination(5, TimeUnit.MINUTES);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	  }
 	
 	
 	public Callable<String> expectDbAsync(String name){
