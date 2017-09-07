8c6272ce4bfcedb601ff34ff2549eb4828467717
zhuhuihui <jackjboss@gmail.com>
2017-09-07 10:24:31 +0800

    add AtomicBoolean AtomicIntegerArray

---
+++ src/main/java/com/gochinatv/cdn/api/jdk/atomic/AtomicBooleanTest.java
@@ -0,0 +1,20 @@
+package com.gochinatv.cdn.api.jdk.atomic;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Test;
+
+/**
+ * 原子boolean
+ * @author jacktomcat
+ *
+ */
+public class AtomicBooleanTest {
+
+	@Test
+	public void test(){
+		AtomicBoolean ab = new AtomicBoolean(true);
+		
+		
+	}
+}
---
+++ src/main/java/com/gochinatv/cdn/api/jdk/atomic/AtomicIntegerArrayTest.java
@@ -0,0 +1,19 @@
+package com.gochinatv.cdn.api.jdk.atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+import org.junit.Test;
+
+/**
+ * 原子 AtomicIntegerArray
+ * @author jacktomcat
+ *
+ */
+public class AtomicIntegerArrayTest {
+
+	@Test
+	public void test(){
+		AtomicIntegerArray aia = new AtomicIntegerArray(10);
+		
+	}
+}
