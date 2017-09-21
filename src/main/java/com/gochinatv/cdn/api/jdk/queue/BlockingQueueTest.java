cd43e867dabead117b901d39f47e569c7f7d2445
zhuhuihui <jackjboss@gmail.com>
2017-09-21 22:47:41 +0800

    add blockingqueue support

--- src/main/java/com/gochinatv/cdn/api/jdk/queue/BlockingQueueTest.java
+++ src/main/java/com/gochinatv/cdn/api/jdk/queue/BlockingQueueTest.java
@@ -23,44 +23,59 @@ public class BlockingQueueTest {
     public void linkedBlockQueueTest() {
 
         //无界队列,最大值是  Integer.MAX_VALUE
-        BlockingQueue<String> queue = new LinkedBlockingQueue<>(1);
+        BlockingQueue<String> queue = new LinkedBlockingQueue<>(10);
+
+        queue.add("aaa");
+        queue.add("bbb");
+
+        //添加元素到队列,有可用空间添加并返回true,否则抛出异常
         boolean add = queue.add("queue01");
         System.out.println("add:"+add + ",size:"+queue.size());
 
         try {
-            queue.put("queue02");
+            queue.put("queue02");//阻塞住,有可用空间才会向里面放
             System.out.println("size:"+queue.size());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        boolean offer = queue.offer("queue03");
+        boolean offer = queue.offer("queue03");//如果有可用空间返回插入并返回true,否则返回false
         System.out.println("offer:"+offer + ",size:"+queue.size());
 
-        String poll = queue.poll();
+        try {
+            //如果有可用空间返回插入并返回true,否则返回false
+            boolean queueOffer2 = queue.offer("queueOffer2", 5, TimeUnit.SECONDS);
+            System.out.println("queueOffer2:"+queueOffer2 + ",size:"+queue.size());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        queue.clear();
+
+        String poll = queue.poll();//获取并移除头元素,如果没有返回null
         System.out.println("poll:"+poll + ",size:"+queue.size());
 
-        String peek = queue.peek();
-        System.out.println("peek:"+peek + ",size:"+queue.size());
-
-        String element = queue.element();
-        System.out.println("element:"+element + ",size:"+queue.size());
-
-        boolean contains = queue.contains("queue03");
-        System.out.println("contains:"+contains + ",size:"+queue.size());
-
         try {
+            //检索并删除此队列的头，如果需要的元素可用，则等待到指定的等待时间。
             String pollTimeOut = queue.poll(5, TimeUnit.SECONDS);
             System.out.println("pollTimeOut:"+pollTimeOut + ",size:"+queue.size());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        try {
-            boolean queueOffer2 = queue.offer("queueOffer2", 5, TimeUnit.SECONDS);
-            System.out.println("queueOffer2:"+queueOffer2 + ",size:"+queue.size());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //获取,但是不移除头部元素
+        String peek = queue.peek();
+        System.out.println("peek:"+peek + ",size:"+queue.size());
+
+        //Retrieves, but does not remove, the head of this queue.
+        // This method differs from peek only in that it throws an exception if this queue is empty.
+        String element = queue.element();
+        System.out.println("element:"+element + ",size:"+queue.size());
+
+        boolean contains = queue.contains("queue03");
+        System.out.println("contains:"+contains + ",size:"+queue.size());
+
+
+
+
 
     }
 
