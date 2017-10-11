3490a12bccd3a16b1b68b2a603fd3788a7c91a2c
zhuhuihui <jackjboss@gmail.com>
2017-10-11 15:15:14 +0800

    add StampedLock

---
+++ src/main/java/com/gochinatv/cdn/api/jdk/concurrent/StampedLockTest.java
@@ -0,0 +1,20 @@
+package com.gochinatv.cdn.api.jdk.concurrent;
+
+import java.util.concurrent.locks.StampedLock;
+
+import org.junit.Test;
+
+/**
+ * http://blog.csdn.net/sunfeizhi/article/details/52135136
+ * @author jacktomcat
+ *
+ */
+public class StampedLockTest {
+
+	@Test
+    public void stampedLock() {
+		StampedLock stampedLock = new StampedLock();
+
+    }
+	
+}
