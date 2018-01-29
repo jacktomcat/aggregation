554df8ffea2ac883b056172ff80f148d16632e5d
zhuhuihui <jackjboss@gmail.com>
2018-01-29 23:00:31 +0800

    add jwt support

--- src/main/java/com/gochinatv/cdn/api/basic/normal/ListTest.java
+++ src/main/java/com/gochinatv/cdn/api/basic/normal/ListTest.java
@@ -1,5 +1,8 @@
 package com.gochinatv.cdn.api.basic.normal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jacktomcat on 17/8/28.
  */
@@ -7,8 +10,17 @@ public class ListTest {
 
 
     public static void main(String[] args) {
+        List<String> data = new ArrayList<>();
+        data.add("1");
+        data.add("2");
+        data.add("3");
+        data.add("4");
+        data.add("5");
+        data.add("6");
+        data.add("7");
 
-
+        List<String> subList = data.subList(6, 7);
+        System.out.println(subList);
     }
 
 
---
+++ src/main/java/com/gochinatv/cdn/api/jwt/JwtUtils.java
@@ -0,0 +1,12 @@
+package com.gochinatv.cdn.api.jwt;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @auhtor jacktomcat
+ * @create 2018-01-29 下午10:58
+ */
+public class JwtUtils {
+
+
+}
