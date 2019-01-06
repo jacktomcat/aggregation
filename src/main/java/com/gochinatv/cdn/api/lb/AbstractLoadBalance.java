05ef979ce2eee41ea495d39ffb8ced7cc816911a
zhuhuihui <jackjboss@gmail.com>
2019-01-06 21:22:12 +0800

    add render

---
+++ src/main/java/com/gochinatv/cdn/api/lb/AbstractLoadBalance.java
@@ -0,0 +1,10 @@
+package com.gochinatv.cdn.api.lb;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @auhtor jacktomcat
+ * @create 2019-01-03 下午10:14
+ */
+public class AbstractLoadBalance {
+}
---
+++ src/main/java/com/gochinatv/cdn/api/lb/LoadBalance.java
@@ -0,0 +1,12 @@
+package com.gochinatv.cdn.api.lb;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @auhtor jacktomcat
+ * @create 2019-01-03 下午10:14
+ */
+public interface LoadBalance {
+
+    //<T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException;
+}
