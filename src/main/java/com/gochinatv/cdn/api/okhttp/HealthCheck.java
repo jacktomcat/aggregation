edb82d5064e0102880df916a3ba859fc3bc0bece
zhuhuihui <jackjboss@gmail.com>
2019-01-08 22:46:15 +0800

    负载均衡

--- src/main/java/com/gochinatv/cdn/api/okhttp/HealthCheck.java
+++ src/main/java/com/gochinatv/cdn/api/okhttp/HealthCheck.java
@@ -1,10 +1,46 @@
 package com.gochinatv.cdn.api.okhttp;
 
+import com.google.common.base.Preconditions;
+import okhttp3.OkHttpClient;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * ${DESCRIPTION}
  *
  * @auhtor jacktomcat
  * @create 2019-01-08 下午10:30
  */
-public class HealthCheck {
+public final class HealthCheck {
+
+    private static OkHttpClient okHttpClient;
+
+    static {
+        if(okHttpClient==null){
+            synchronized (OkHttpClient.class) {
+                if (okHttpClient == null) {
+                    okHttpClient = new OkHttpClient()
+                            .newBuilder()
+                            .readTimeout(10, TimeUnit.SECONDS)
+                            .writeTimeout(10,TimeUnit.SECONDS)
+                            .connectTimeout(10,TimeUnit.SECONDS).build();
+                }
+            }
+        }
+    }
+
+    /**
+     * 健康检查
+     */
+    public static void check(Resource resource){
+        Preconditions.checkArgument(CollectionUtils.isNotEmpty(resource.getMachines()),"调用机器不能为空");
+        String strategy = resource.getStrategy();
+        int size = resource.getMachines().size();
+
+
+
+    }
+
+
 }
